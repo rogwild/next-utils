77df16fe5b5e732ce18e5ab8ff0b1711f08a984b
rogwild <rogwild.design@gmail.com>
2022-08-24 13:31:13 +0300

    new version

--- dist/api/index.d.ts
+++ dist/api/index.d.ts
@@ -2,7 +2,10 @@ import { Api } from "./client";
 export declare const getImageUrl: (obj: {
     formats?: object;
     url: string;
-}, size: string, BACKEND_URL: string) => string;
+}, options?: {
+    size?: string;
+    BACKEND_URL?: string;
+}) => string;
 export declare const transformPageBlock: (block: any, transformers: any) => any;
 export declare const removeEmptyFields: ({ data, passKey, files }: {
     data: any;
@@ -10,5 +13,29 @@ export declare const removeEmptyFields: ({ data, passKey, files }: {
     files: any;
 }) => any;
 export declare const appendFilesToFormData: (formData: any, files: any) => void;
+/**
+ * Get data for Next.js page form Strapi API
+ *
+ * @param additionalBlocks - ex. ['header', 'footer']
+ * @param transformers - ex.
+ * { [`page-blocks.main-block`]: (block) => {
+ *   return {
+ *      ...block,
+ *      images: block?.images?.map((image) =>
+ *              getImageUrl(image, {BACKEND_URL: "http://localhost:1337"})) || null,
+ *            };
+ *    },
+ * }
+ */
+export declare const getPageData: (params: {
+    url?: string;
+    page: string;
+    locale?: string;
+    keys: string[];
+    additionalBlocks?: string[];
+    transformers?: {};
+}) => Promise<{
+    pageBlocks: any;
+}>;
 export declare const ApiClient: typeof Api;
 export declare const transformResponseItem: (resItem: any) => any;
--- dist/index.js
+++ dist/index.js
@@ -35,6 +35,44 @@ var __assign = function() {
     return __assign.apply(this, arguments);
 };
 
+function __awaiter(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+}
+
+function __generator(thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+}
+
 function __spreadArray(to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {
@@ -134,20 +172,21 @@ var Api = /** @class */ (function () {
     return Api;
 }());
 
-// import qs from "qs";
-var getImageUrl = function (obj, size, BACKEND_URL) {
+var _a;
+var getImageUrl = function (obj, options) {
     var _a, _b;
-    if (size === void 0) { size = "medium"; }
+    if (options === void 0) { options = {}; }
+    var size = options.size, BACKEND_URL = options.BACKEND_URL;
     if (!obj) {
         return null;
     }
-    var url = ((_b = (_a = obj.formats) === null || _a === void 0 ? void 0 : _a[size]) === null || _b === void 0 ? void 0 : _b.url) || obj.url;
-    return "".concat(BACKEND_URL).concat(url);
+    var url = size ? ((_b = (_a = obj.formats) === null || _a === void 0 ? void 0 : _a[size]) === null || _b === void 0 ? void 0 : _b.url) || obj.url : obj.url;
+    return "".concat(BACKEND_URL || "").concat(url);
 };
 var transformPageBlock = function (block, transformers) {
     var key = block === null || block === void 0 ? void 0 : block._Component;
     if (!(transformers === null || transformers === void 0 ? void 0 : transformers[key])) {
-        return null;
+        return block;
     }
     return transformers[key](block);
 };
@@ -204,6 +243,68 @@ var appendFilesToFormData = function (formData, files) {
         }
     }
 };
+(_a = {},
+    _a["page-blocks.main-block"] = function (block) {
+        var _a;
+        return __assign(__assign({}, block), { images: ((_a = block === null || block === void 0 ? void 0 : block.images) === null || _a === void 0 ? void 0 : _a.map(getImageUrl)) || null });
+    },
+    _a);
+/**
+ * Get data for Next.js page form Strapi API
+ *
+ * @param additionalBlocks - ex. ['header', 'footer']
+ * @param transformers - ex.
+ * { [`page-blocks.main-block`]: (block) => {
+ *   return {
+ *      ...block,
+ *      images: block?.images?.map((image) =>
+ *              getImageUrl(image, {BACKEND_URL: "http://localhost:1337"})) || null,
+ *            };
+ *    },
+ * }
+ */
+var getPageData = function (params) { return __awaiter(void 0, void 0, void 0, function () {
+    var url, locale, _a, keys, page, _b, transformers, _c, additionalBlocks, client, additionalPopulate, _i, additionalBlocks_1, block, query, res, pageBlocks;
+    var _d;
+    return __generator(this, function (_e) {
+        switch (_e.label) {
+            case 0:
+                url = params.url, locale = params.locale, _a = params.keys, keys = _a === void 0 ? [] : _a, page = params.page, _b = params.transformers, transformers = _b === void 0 ? {} : _b, _c = params.additionalBlocks, additionalBlocks = _c === void 0 ? [] : _c;
+                client = new Api(url ? url : undefined);
+                additionalPopulate = {};
+                if (additionalBlocks) {
+                    for (_i = 0, additionalBlocks_1 = additionalBlocks; _i < additionalBlocks_1.length; _i++) {
+                        block = additionalBlocks_1[_i];
+                        additionalPopulate[block] = {
+                            populate: __assign({}, keys.reduce(function (a, b) {
+                                var _a;
+                                return (__assign(__assign({}, a), (_a = {}, _a[b] = { populate: "*" }, _a)));
+                            }, {})),
+                        };
+                    }
+                }
+                query = {
+                    locale: locale,
+                    populate: __assign({ page_blocks: {
+                            populate: __assign({}, keys.reduce(function (a, b) {
+                                var _a;
+                                return (__assign(__assign({}, a), (_a = {}, _a[b] = { populate: "*" }, _a)));
+                            }, {})),
+                        } }, additionalPopulate),
+                };
+                return [4 /*yield*/, client.request({
+                        model: page,
+                        query: query,
+                    })];
+            case 1:
+                res = _e.sent();
+                pageBlocks = (_d = res === null || res === void 0 ? void 0 : res.pageBlocks) === null || _d === void 0 ? void 0 : _d.map(function (block) {
+                    return transformPageBlock(block, transformers);
+                });
+                return [2 /*return*/, { pageBlocks: pageBlocks }];
+        }
+    });
+}); };
 var ApiClient = Api;
 var transformResponseItem = transformResponseItem$1;
 
@@ -213,6 +314,7 @@ var apiUtils = /*#__PURE__*/Object.freeze({
     transformPageBlock: transformPageBlock,
     removeEmptyFields: removeEmptyFields,
     appendFilesToFormData: appendFilesToFormData,
+    getPageData: getPageData,
     ApiClient: ApiClient,
     transformResponseItem: transformResponseItem
 });
